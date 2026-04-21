694932aa6b34a25d9237cd525336ee2501e3757a
weiiwu <master_wu51@hotmail.com>
2026-04-21 11:34:42 +1000

    feat: clerk auth implementation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,33 @@
 import { tabs } from "@/constance/data";
 import { colors, components } from "@/constance/theme";
+import { useAuth } from "@clerk/expo";
 import clsx from "clsx";
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const tabBar = components.tabBar;
 
+const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+        <View className="tabs-icon">
+            <View className={clsx('tabs-pill', focused && 'tabs-active')}>
+                <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+            </View>
+        </View>
+    )
+}
+
 const TabLayout = () => {
+    const { isSignedIn, isLoaded } = useAuth();
     const insets = useSafeAreaInsets();
 
-    const TabIcon = ({ focused, icon }: TabIconProps) => {
-        return (
-            <View className="tabs-icon">
-                <View className={clsx('tabs-pill', focused && 'tabs-active')}>
-                    <Image source={icon} resizeMode="contain" className="tabs-glyph" />
-                </View>
-            </View>
-        )
+    if (!isLoaded) {
+        return null;
+    }
+
+    if (!isSignedIn) {
+        return <Redirect href="/(auth)/sign-in" />;
     }
 
     return (
