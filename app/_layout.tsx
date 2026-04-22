1e71672bdf1bd211953bb6ff3faf826b491f235a
weiiwu <master_wu51@hotmail.com>
2026-04-22 14:23:53 +1000

    posthog added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider, useAuth } from '@clerk/expo';
 import { tokenCache } from '@clerk/expo/token-cache';
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
+import { PostHogProvider } from 'posthog-react-native';
 import { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
@@ -40,9 +41,13 @@ function RootLayoutContent() {
 
 export default function RootLayout() {
   return (
-
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <RootLayoutContent />
-    </ClerkProvider>
+    <PostHogProvider
+      apiKey={process.env.EXPO_PUBLIC_POSTHOG_API_KEY!}
+      options={{ host: process.env.EXPO_PUBLIC_POSTHOG_HOST }}
+    >
+      <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+        <RootLayoutContent />
+      </ClerkProvider>
+    </PostHogProvider>
   );
 }
